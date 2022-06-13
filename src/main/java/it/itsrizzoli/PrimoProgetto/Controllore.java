4e6461b14c0198b4bace259cdeedb9227f2b817d
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-06-13 15:54:00 +0200

    feat: added switch for better code

--- src/main/java/it/itsrizzoli/PrimoProgetto/Controllore.java
+++ src/main/java/it/itsrizzoli/PrimoProgetto/Controllore.java
@@ -7,46 +7,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Controllore {
-        @GetMapping("/somma")
-        public String Somma(@RequestParam ("val") int val1,@RequestParam ("val2") int val2, Model model) {
-            int somma=val1+val2;
-            model.addAttribute("operazione", "somma");
-            model.addAttribute("result", somma);
+        @GetMapping("/operazione")
+        public String Operazione(@RequestParam ("val") int val1,@RequestParam ("val2") int val2,@RequestParam("op") String op, Model model) {
+            double risultato=0;
+            switch (op){
+                case "somma":
+                    risultato=val1+val2;
+                    model.addAttribute("operazione", "somma");
+                    break;
+
+                case "differenza":
+                    risultato=val1-val2;
+                    break;
+
+                case "prodotto":
+                    risultato=val1*val2;
+                    break;
+
+                case "divisione":
+                    risultato=(double)val1/ (double)val2;
+                    break;
+
+                case "potenza":
+                    int a=val1;
+                    int el=1;
+                    for (int i = 0; i < val2; i++) {
+                        el=el*a;
+                    }
+                    risultato=el;
+                    break;
+
+                default:
+                    break;
+            }
+
+            model.addAttribute("result", risultato);
             return "result";
         }
-    @GetMapping("/prodotto")
-    public String Prodotto(@RequestParam ("val") int val1,@RequestParam ("val2") int val2, Model model) {
-        int pr=val1*val2;
-        model.addAttribute("operazione", "prodotto");
-        model.addAttribute("result", pr);
-        return "result";
-    }
-    @GetMapping("/divisione")
-    public String Divisione(@RequestParam ("val") int val1,@RequestParam ("val2") int val2, Model model) {
-        double div=(double)val1/ (double)val2;
-        model.addAttribute("operazione", "divisione");
-        model.addAttribute("result", div);
-        return "result";
-    }
-    @GetMapping("/differenza")
-    public String differenza(@RequestParam ("val") int val1,@RequestParam ("val2") int val2, Model model) {
-        int div=val1-val2;
-        model.addAttribute("operazione", "differenza");
-        model.addAttribute("result", div);
-        return "result";
-    }
-    @GetMapping("/potenza")
-    public String Potenza(@RequestParam ("val") int val1,@RequestParam ("val2") int val2, Model model) {
-        int a=val1;
-        int el=1;
-        for (int i = 0; i < val2; i++) {
-            el=el*a;
-        }
-        model.addAttribute("operazione", "potenza");
-        model.addAttribute("result", el);
-        return "result";
-    }
-
 
 
 }
